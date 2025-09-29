dcb0322bc3a25c7975dc125ca66a2d2426b9d4ea
halfdem0n <108952600+halfdem0n@users.noreply.github.com>
2025-09-29 09:25:50 +0200

    Delete menu.js

--- menu.js
+++
@@ -1,7 +0,0 @@
-const hamMenu = document.querySelector('.ham-menu');
-const ul = document.querySelector('.ul');
-
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');
-    ul.classList.toggle('active');
-})
